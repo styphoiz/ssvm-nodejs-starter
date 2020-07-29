1a001851e44641127dc2ddffca269071f3360446
Mohammed Hoosain <styphoiz@gmail.com>
2020-07-29 11:43:06 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,23 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn say(s: &str) -> String {
-  println!("The Rust function say() received {}", s);
-  let r = String::from("hello ");
-  return r + s;
+//pub fn say(s: &str) -> String {
+//  println!("The Rust function say() received {}", s);
+//  let r = String::from("hello ");
+//  return r + s;
+//}
+
+
+fn say(a: i32, i: i32, p: i32) -> String {
+  let r = String::from("Interest calculation is : ");
+  let mut calc = 0;
+  let mut aa = a;
+  for x in (0..p).enumerate() {
+    calc =(aa*i/100) + aa;
+    aa = calc;
+}
+let r = String::from("Interest calculation is : ");
+let result = format!("{} {}", r, calc);
+//r=r+&calc.to_string();
+  return result;
 }
